3a3b5b141784411919bc5188587bf8397f4bf027
Siddhant Gautam <43059326+sgautam8434@users.noreply.github.com>
2024-08-16 09:38:15 +0530

    project setup

---
+++ src/main/java/com/intuit/gaming_service/GamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.gaming_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intuit/gaming_service/GamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.gaming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
